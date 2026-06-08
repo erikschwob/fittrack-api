c3c219a300cbf8ea0b95ed6cc4c27fc1d2f11c79
Erik Schwob <erikdrache11@gmail.com>
2026-06-08 21:38:18 +0000

    chore: scaffold Spring Boot 3 project (Java 17, Maven wrapper)

---
+++ src/main/java/de/erikschwob/fittrack/FittrackApiApplication.java
@@ -0,0 +1,12 @@
+package de.erikschwob.fittrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FittrackApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FittrackApiApplication.class, args);
+    }
+}
